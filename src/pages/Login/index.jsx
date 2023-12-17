69c70e03fcf8b7022025ff1c400ac07ff5c10938
eMoonT <1163060734@qq.com>
2023-12-17 12:48:45 +0800

    login page && request utils

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,52 @@
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
+
+const Login = () => {
+  const onFinish = (value) => {
+    console.log(value)
+  };
+
+  return (
+    <div className="login flex items-center justify-center h-screen bg-[url('./assets/login.png')] bg-left-top bg-cover">
+      <Card className="login-container shadow-xl flex justify-center">
+        <img className="login-logo w-[350px] h-[90px]" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger='onBlur'>
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: "请输入手机号",
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号",
+              },
+            ]}
+          >
+            <Input size="large" className="w-full" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: "请输入验证码",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 配置别名
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"), // @代替src
+      "#": path.resolve("./types"), // #代替types
+    },
+  },
 })
