ce3c127aa6a661510affebe1769886db56f2b2fd
eMoonT <1163060734@qq.com>
2023-12-17 15:18:33 +0800

    add layout

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,13 +1,73 @@
-import { useEffect } from "react"
-import { request } from "../../utils"
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons'
+import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
-const Layout = () => {
-  useEffect(() => {
-    request.get('/user/profile')
-  },[])
+const { Header, Sider } = Layout
+
+const items = [
+  {
+    label: '首页',
+    key: '/',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: '文章管理',
+    key: '/article',
+    icon: <DiffOutlined />,
+  },
+  {
+    label: '创建文章',
+    key: '/publish',
+    icon: <EditOutlined />,
+  },
+]
+
+const GeekLayout = () => {
+  const navigate = useNavigate()
+  const onMenuClick =(route) => {
+    const path = route.key
+    navigate(path)
+  }
+
+  const location = useLocation()
+  const selectedKeys = [location.pathname]
+
   return (
-    <div>Layout</div>
+    <Layout className='w-screen h-screen'>
+      <Header className="header p-0 flex items-center justify-between">
+        <div className="logo w-[200px] h-[60px] bg-cover bg-[url('./assets/logo.png')] " />
+        <div className="user-info pr-5 space-x-2">
+          <span className="user-name text-white">柴柴老师</span>
+          <span className="user-logout text-white">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            onClick={onMenuClick}
+            mode="inline"
+            theme="dark"
+            selectedKeys={selectedKeys}
+            defaultSelectedKeys={['1']}
+            items={items}
+            style={{ height: '100%', borderRight: 0 }}></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          <Outlet />
+        </Layout>
+      </Layout>
+    </Layout>
   )
 }
-
-export default Layout
+export default GeekLayout
