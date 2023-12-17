64e78cbcbc16a7ed02331f9b1bea5721662f6ca0
eMoonT <1163060734@qq.com>
2023-12-17 12:58:53 +0800

    config store

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import '@/index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
+import store from '@/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./modules/user";
+
+const store = configureStore({
+    reducer: userReducer
+})
+
+export default store
